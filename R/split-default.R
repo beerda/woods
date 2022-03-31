944a37928ca6be6b2ee1b71c8c3ad0e442613713
Michal Burda <michal.burda@osu.cz>
2022-03-31 09:46:06 +0200

    Test of split.default()

--- R/split-default.R
+++ R/split-default.R
@@ -1,8 +1,16 @@
+#' Split data to `left` and `right` part accordingly to the logical vector
 #'
-#' @return
+#' @param def a logical vector indicating which data rows belong to the `left` part of the split
+#' @param data the instance of the `woods_data` S3 class
+#' @param cfg the configuration list (not needed for this function)
+#' @return a list with `left` and `right` data (`left` data are those indicated as `TRUE` in the
+#'     `def` vector, `right` data correspond to `FALSE` in the `def` vector)
 #' @author Michal Burda
 #' @export
 split.default <- function(def, data, cfg) {
+    assert_that(is.vector(def) && is.logical(def))
+    assert_that(is.woods_data(data))
+
     list(left = data[def, ],
          right = data[!def, ])
 }
---
+++ tests/testthat/test-split.R
@@ -0,0 +1,17 @@
+test_that("split.default", {
+    y <- 1:5
+    x <- data.frame(a = 2 * (1:5), b = 3 * (1:5))
+    d <- woods_data(y = y, x = x)
+    cfg <- list()
+
+    i <- c(T, T, F, T, F)
+    r <- split(i, d, cfg)
+
+    expect_true(is.list(r))
+    expect_true(is.woods_data(r$left))
+    expect_true(is.woods_data(r$right))
+    expect_equal(r$left$x, x[i, , drop=FALSE])
+    expect_equal(r$left$y, y[i])
+    expect_equal(r$right$x, x[!i, , drop=FALSE])
+    expect_equal(r$right$y, y[!i])
+})
