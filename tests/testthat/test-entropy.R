a067336942bbec0b939eb21facc95c85b19ad094
Michal Burda <michal.burda@osu.cz>
2022-04-26 15:19:59 +0200

    Removed entropy() and information_gain() because the latter is wrong and both will be computed in the different way in ig_split()

--- tests/testthat/test-entropy.R
+++
@@ -1,8 +0,0 @@
-test_that('entropy', {
-  expect_equal(entropy(c(rep(1, 5), rep(2, 25))),
-               0.65, tolerance = 0.0001)
-  expect_equal(entropy(c(rep(1, 20), rep(2, 20))),
-               1)
-  expect_equal(entropy(c(rep(1, 20), rep(2, 0))),
-               0)
-})
--- tests/testthat/test-information_gain.R
+++
@@ -1,5 +0,0 @@
-test_that('information_gain', {
-  expect_equal(information_gain(y = c(rep(1, 5), rep(2, 25), rep(1, 20)),
-                                by = c(rep(TRUE, 30), rep(FALSE, 20))),
-               0.39, tolerance = 0.0001)
-})
