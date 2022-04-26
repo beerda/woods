5d805583e034e4c06d48bd0f1e0102c1ea53f609
Michal Burda <michal.burda@osu.cz>
2022-04-26 12:23:05 +0200

    Fixed mode_result test

--- tests/testthat/test-mode_result.R
+++ tests/testthat/test-mode_result.R
@@ -4,6 +4,6 @@ test_that("mode_result", {
 
     r <- mode_result(d)
     expect_true(is.list(r))
-    expect_equal(r$label, 'mode 2')
+    expect_equal(r$label, '2 (mode)')
     expect_equal(r$value, 2)
 })
