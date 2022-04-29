b59878562c02bcfc4c7fe46434bc36fc2d2935bf
Michal Burda <michal.burda@osu.cz>
2022-04-29 09:52:34 +0200

    Removed unnecessary split variants

--- R/split-ig_split.R
+++
@@ -1,9 +0,0 @@
-#'
-#' @return
-#' @author Michal Burda
-#' @export
-split.ig_split <- function(def, data, cfg) {
-    indices <- data$x[[def$by]] <= def$cutpoint
-
-    split(indices, data, cfg)
-}
