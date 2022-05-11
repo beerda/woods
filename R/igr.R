36e8916d00122b8be1b27ea30928af78dd0bc5ae
Michal Burda <michal.burda@osu.cz>
2022-05-11 08:20:12 +0200

    Fixed bug caused by concatenation of factors

--- R/igr.R
+++ R/igr.R
@@ -3,9 +3,10 @@
 #' @author Michal Burda
 #' @export
 #' @seealso https://scientistcafe.com/ids/splitting-criteria.html
+#' @import forcats
 igr <- function(x1, x2) {
     prop <- length(x1) / (length(x1) + length(x2))
-    before <- entropy(c(x1, x2))
+    before <- entropy(fct_c(x1, x2))
     after <- prop * entropy(x1) + (1 - prop) * entropy(x2)
     x <- factor(c(rep(1, length(x1)), rep(0, length(x2))), levels = 0:1)
     si <- entropy(x)
