34121c2af933652059ad989a7605a4cf740c2987
Michal Burda <michal.burda@osu.cz>
2022-08-17 12:02:18 +0200

    Updated caret model info accordingly to randomForest

--- R/woods_model_info.R
+++ R/woods_model_info.R
@@ -1,14 +1,14 @@
-.miParameters <- data.frame(parameter = c('mtry', 'principal_component', 'linear_model', 'hypersphere', 'f_transform'),
-                            class = c('numeric', 'logical', 'logical', 'logical', 'logical'),
-                            label = c('mtry', 'principal_component', 'linear_model', 'hypersphere', 'f_transform'))
+.miParameters <- data.frame(parameter = c('mtry'), #, 'principal_component', 'linear_model', 'hypersphere', 'f_transform'),
+                            class = c('numeric'), #, 'logical', 'logical', 'logical', 'logical'),
+                            label = c('mtry')) #, 'principal_component', 'linear_model', 'hypersphere', 'f_transform'))
 
 
 .miGrid <- function(x, y, len = NULL, search = 'grid') {
-    expand.grid(mtry = 2:5,
-                principal_component = TRUE,
-                linear_model = TRUE,
-                hypersphere = TRUE,
-                f_transform = TRUE)
+    expand.grid(mtry = caret::var_seq(p = ncol(x), classification = is.factor(y), len = len))
+                #principal_component = FALSE,
+                #linear_model = FALSE,
+                #hypersphere = FALSE,
+                #f_transform = FALSE)
 }
 
 
@@ -17,10 +17,11 @@
                  x = as.data.frame(x),
                  n_tree = 500,
                  mtry = param$mtry,
-                 principal_component = param$principal_component,
-                 linear_model = param$linear_model,
-                 hypersphere = param$hypersphere,
-                 f_transform = param$f_transform)
+                 ...)
+                 #principal_component = param$principal_component,
+                 #linear_model = param$linear_model,
+                 #hypersphere = param$hypersphere,
+                 #f_transform = param$f_transform)
 }
 
 
