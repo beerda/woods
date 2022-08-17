45ada130727e86cd46575b39519197b6b8346b52
Michal Burda <michal.burda@osu.cz>
2022-08-17 12:02:30 +0200

    Fixes in fuzzy_transform

--- R/fuzzy_transform.R
+++ R/fuzzy_transform.R
@@ -4,20 +4,27 @@
 #' @export
 #' @import lfl
 fuzzy_transform <- function(data) {
+    partitions <- 2
+
+    if (nrow(data$x) < partitions^ncol(data$x)) {
+        return(NULL)
+    }
+
     y <- data$y
     levs <- NULL
     if (is.factor(y)) {
         levs <- levels(y)
         i <- runif(length(levs))
         i[which.max(i)] <- 1    # ensure at least one level is selected
+        i[which.min(i)] <- 0    # ensure at least one level is not selected
         levs <- levs[i > 0.5]
         y <- as.numeric(y %in% levs)
     }
 
-    breaks <- lapply(data$x, equifreq, 3, 'infinity', 'infinity')
+    breaks <- lapply(data$x, equifreq, partitions, 'infinity', 'infinity')
     x <- as.matrix(data$x)
     xmemb <- fcut(x, breaks = breaks)
-    fit <- ft(x, xmemb, y, order = 0)
+    fit <- ft(x, xmemb, y, order = 1)
 
     structure(list(fit = fit,
                    breaks = breaks,
